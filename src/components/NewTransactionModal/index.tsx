7c4226420e80217977abe675736d416f4046dbeb
Julie Moura <julie.moura@squair.io>
2022-02-11 13:32:40 -0300

    feat: saving data

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import { useState, FormEvent } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,13 +12,28 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  /**criamos cada estado para armazenar cada uma das informacoes que queremos salvar da nova transacao */
+  const [title, setTitle] = useState(" ");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState(" ");
+  
   /**state para armazenar o botão que o usuário clicou */
   const [type, setType] = useState('deposit');
   /**começamos com deposit, pois é o tipo que defini la do index.tsx  */
 
   /**handle para criar uma nova transacao e salvar os dados da mesma */
+  /***/
   function handleCreateNewTransaction(e: FormEvent){
     e.preventDefault();
+
+    /**criando uma const data para salvar os dados do formulario */
+    const data = {
+      title,
+      value, 
+      category,
+      type
+    };
+
   }
 
   return (
@@ -43,9 +59,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transacao</h2>
 
-        <input placeholder="Título" />
+        <input 
+          placeholder="Título" 
+          value={title} /**para salvar no estado que criammos. */
+          onChange={event => setTitle(event.target.value)} /**para salvar no novo state setTitle, o valor que foi digitado no input */
+        /> 
 
-        <input type="number" placeholder="Valor" />
+        <input 
+          type="number" 
+          placeholder="Valor" 
+          value={value} /**para salvar no estado que criammos. */
+          onChange={event => setValue(Number(event.target.value))} /**para salvar no novo state setTitle, o valor que foi digitado no input */
+                                                                  /**aqui utilizamos o Number para converter o valor para numérico */
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -69,7 +95,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input 
+          placeholder="Categoria" 
+          value={category} /**para salvar no estado que criammos. */
+          onChange={event => setCategory(event.target.value)} /**para salvar no novo state setTitle, o valor que foi digitado no input */
+        />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,8 @@ createServer({
           createdAt: new Date()
         }
       ]
-    });
+    })
+
   }
 })
 
